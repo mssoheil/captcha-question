9bd0bb0730965268b8c5bd5dc9b913ef1f0e6f90
mssoheil88 <mssoheil88@yahoo.com>
2019-11-30 12:48:49 +0330

    feat: check validate on submit

--- src/components/captcha-content/index.tsx
+++ src/components/captcha-content/index.tsx
@@ -6,162 +6,200 @@ import AntdCheckBox from "../ant-checkbox";
 // types
 
 interface NormalizeData {
-	clause: string;
-	isCorrect: boolean;
+  clause: string;
+  isCorrect: boolean;
 }
 interface IProps {
-	randomNumber: number;
-	isCorrect: boolean;
-	correctData: any[];
-	uncorrectData: any[];
-	maxNumberOfItems: number;
+  randomNumber: number;
+  isCorrect: boolean;
+  correctData: any[];
+  uncorrectData: any[];
+  maxNumberOfItems: number;
+  isHuman: boolean;
+  setIsHuman: (isHuman: boolean) => void;
 }
 interface IState {
-	dataArray: any[];
-	correctNormalizedDataArray: any[];
-	uncorrectNormalizedDataArray: any[];
-	checkboxTruthArray: any[];
-	checkboxFalsyArray: any[];
+  dataArray: any[];
+  correctNormalizedDataArray: any[];
+  uncorrectNormalizedDataArray: any[];
+  checkboxTruthArray: any[];
+  checkboxFalsyArray: any[];
+  selectedCheckboxes: any[];
+  isHuman: boolean;
 }
 // styles
 const Wrapper = styled.div`
-	display: flex;
-	justify-content: flex-start;
-	flex-direction: column;
-	width: 300px;
-	margin: 0 auto;
-	.ant-checkbox-wrapper {
-		text-align: left;
-		margin-left: 0;
-		padding-left: 20px;
-	}
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+  .ant-checkbox-wrapper {
+    text-align: left;
+    margin-left: 0;
+    padding-left: 20px;
+  }
+`;
+
+const CheckBoxWrapper = styled.div<any>`
+  text-align: left;
+  display: flex;
+  margin-bottom: 5px;
+  align-items: center;
+  input {
+    margin-right: 5px;
+  }
 `;
 
 function reducer(state: any, action: any) {
-	return { ...state, ...action };
+  return { ...state, ...action };
 }
 
 const CaptchaTitle: React.FC<IProps> = props => {
-	const { randomNumber, isCorrect, correctData, uncorrectData, maxNumberOfItems } = props;
+  const {
+    randomNumber,
+    isCorrect,
+    correctData,
+    uncorrectData,
+    maxNumberOfItems,
+    isHuman,
+    setIsHuman
+  } = props;
 
-	const initialState = (): IState => ({
-		dataArray: [],
-		correctNormalizedDataArray: [],
-		uncorrectNormalizedDataArray: [],
-		checkboxTruthArray: [],
-		checkboxFalsyArray: [],
-	});
-	const [state, dispatch] = React.useReducer(reducer, initialState());
+  const initialState = (): IState => ({
+    dataArray: [],
+    correctNormalizedDataArray: [],
+    uncorrectNormalizedDataArray: [],
+    checkboxTruthArray: [],
+    checkboxFalsyArray: [],
+    selectedCheckboxes: [],
+    isHuman: false
+  });
+  const [state, dispatch] = React.useReducer(reducer, initialState());
 
-	/**
-	 * Shuffles array in place. ES6 version
-	 * @param {Array} a items An array containing the items.
-	 */
-	function shuffle(a: any[]): any[] {
-		for (let i = a.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[a[i], a[j]] = [a[j], a[i]];
-		}
-		return a;
-	}
+  /**
+   * Shuffles array in place. ES6 version
+   * @param {Array} a items An array containing the items.
+   */
+  function shuffle(a: number[]): number[] {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
 
-	function getRndInteger(min: number, max: number): number {
-		return Math.floor(Math.random() * (max - min)) + min;
-	}
+  function getRndInteger(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-	function normalizeArray(array: any[], isCorrect: boolean) {
-		const normalized = array.map((item: any) => {
-			let normalizedArr = item;
-			if (item.clause) {
-				normalizedArr = {
-					clause: item.clause,
-					isCorrect,
-				};
-			} else if (typeof item === "string") {
-				normalizedArr = {
-					clause: item,
-					isCorrect,
-				};
-			}
-			return normalizedArr;
-		});
-		return normalized;
-	}
+  function normalizeArray(array: any[], isCorrect: boolean) {
+    const normalized = array.map((item: any) => {
+      let normalizedArr = item;
+      if (item.clause) {
+        normalizedArr = {
+          clause: item.clause,
+          isCorrect
+        };
+      } else if (typeof item === "string") {
+        normalizedArr = {
+          clause: item,
+          isCorrect
+        };
+      }
+      return normalizedArr;
+    });
+    return normalized;
+  }
 
-	/**
-	 * Generate random 16 radix via `Math.random` and convert a `string` then take the third character
-	 *
-	 * @returns {string}
-	 */
-	function randomString(): string {
-		return Math.random()
-			.toString(16)
-			.slice(2);
-	}
+  /**
+   * Generate random 16 radix via `Math.random` and convert a `string` then take the third character
+   *
+   * @returns {string}
+   */
+  function randomString(): string {
+    return Math.random()
+      .toString(16)
+      .slice(2);
+  }
 
-	React.useEffect(() => {
-		const tempCorrectData: any[] = normalizeArray(correctData, true);
-		const tempUncorrectData: any[] = normalizeArray(uncorrectData, false);
+  React.useEffect(() => {
+    const tempCorrectData: any[] = normalizeArray(correctData, true);
+    const tempUncorrectData: any[] = normalizeArray(uncorrectData, false);
 
-		dispatch({
-			correctNormalizedDataArray: tempCorrectData,
-			uncorrectNormalizedDataArray: tempUncorrectData,
-		});
-		const randomNum = getRndInteger(randomNumber, maxNumberOfItems - 1);
+    dispatch({
+      correctNormalizedDataArray: tempCorrectData,
+      uncorrectNormalizedDataArray: tempUncorrectData
+    });
+    const randomNum = getRndInteger(randomNumber, maxNumberOfItems - 1);
 
-		const tempDataArray = [];
+    const tempDataArray = [];
 
-		const truthyData = isCorrect ? tempCorrectData : tempUncorrectData;
-		const falsyData = !isCorrect ? tempCorrectData : tempUncorrectData;
+    const truthyData = isCorrect ? tempCorrectData : tempUncorrectData;
+    const falsyData = !isCorrect ? tempCorrectData : tempUncorrectData;
 
-		for (let i = 0; i < randomNum; i++) {
-			tempDataArray.push(truthyData[getRndInteger(0, truthyData.length)]);
-		}
-		for (let i = 0; i < maxNumberOfItems - randomNum; i++) {
-			tempDataArray.push(falsyData[getRndInteger(0, falsyData.length)]);
-		}
+    for (let i = 0; i < randomNum; i++) {
+      tempDataArray.push(truthyData[getRndInteger(0, truthyData.length)]);
+    }
+    for (let i = 0; i < maxNumberOfItems - randomNum; i++) {
+      tempDataArray.push(falsyData[getRndInteger(0, falsyData.length)]);
+    }
 
-		dispatch({ dataArray: shuffle(tempDataArray) });
+    dispatch({ dataArray: shuffle(tempDataArray) });
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isCorrect, correctData, uncorrectData]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCorrect, correctData, uncorrectData]);
 
-	function handleCheckBoxChange(status: boolean, value: NormalizeData) {
-		console.log("DEBUG: handleCheckBoxChange -> status", status);
-		const truthyArray = state.checkboxTruthArray;
-		const falsyArray = state.checkboxFalsyArray;
+  function handleCheckBoxChange(
+    status: boolean,
+    { item, index }: { item: NormalizeData; index: number }
+  ) {
+    if (state.selectedCheckboxes.find((item: any) => item.index === index)) {
+      dispatch({
+        selectedCheckboxes: state.selectedCheckboxes.filter(
+          (item: any) => item.index !== index
+        )
+      });
+    } else {
+      dispatch({
+        selectedCheckboxes: state.selectedCheckboxes.concat([{ item, index }])
+      });
+    }
+  }
 
-		if (value.isCorrect === isCorrect) {
-			if (status) {
-				truthyArray.push(value.clause);
-			} else {
-				truthyArray.pop();
-			}
-		} else {
-			if (status) {
-				falsyArray.push(value.clause);
-			} else {
-				falsyArray.pop();
-			}
-		}
-		dispatch({
-			checkboxTruthArray: truthyArray,
-			checkboxFalsyArray: falsyArray,
-		});
-	}
+  function handleClick() {
+    const truthyItems = state.selectedCheckboxes.filter(
+      (item: any) => item.item.isCorrect === isCorrect
+    );
+    const falsyItems = state.selectedCheckboxes.filter(
+      (item: any) => item.item.isCorrect !== isCorrect
+    );
+    setIsHuman(falsyItems.length === 0 && truthyItems.length === randomNumber);
+  }
 
-	console.log("DEBUG: dataArray", state.dataArray);
-	return (
-		<Wrapper>
-			{state.dataArray &&
-				state.dataArray.map((item: NormalizeData, index: number) => (
-					<AntdCheckBox key={randomString()} onChange={e => handleCheckBoxChange(e.target.checked, item)}>
-						{item.clause}
-					</AntdCheckBox>
-				))}
-			{console.log(state.checkboxFalsyArray)}
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      {state.dataArray &&
+        state.dataArray.map((item: NormalizeData, index: number) => (
+          <CheckBoxWrapper key={randomString()}>
+            <input
+              id={`checkbox-item__${index}`}
+              type="checkbox"
+              checked={Boolean(
+                state.selectedCheckboxes.find(
+                  (item: any) => item.index === index
+                )
+              )}
+              onChange={e =>
+                handleCheckBoxChange(e.target.checked, { item, index })
+              }
+            />
+            {item.clause}
+          </CheckBoxWrapper>
+        ))}
+      <button onClick={handleClick}>submit</button>
+    </Wrapper>
+  );
 };
 
 export default CaptchaTitle;
--- src/components/captcha/index.tsx
+++ src/components/captcha/index.tsx
@@ -9,55 +9,69 @@ import correctData from "../../data/correct-data.json";
 import uncorrectData from "../../data/uncorrect-data.json";
 // styles
 const Wrapper = styled.div`
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	padding-top: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: 20px;
 `;
 // types
 interface IProps {
-	maxNumberOfItems: number;
+  maxNumberOfItems: number;
 }
 
 interface IState {
-	randomNumber: number;
-	isCorrect: boolean;
+  randomNumber: number;
+  isCorrect: boolean;
+  isHuman: boolean;
 }
 
 function reducer(state: any, action: any) {
-	return { ...state, ...action };
+  return { ...state, ...action };
 }
 
 const Captcha: React.FC<IProps> = props => {
-	const { maxNumberOfItems } = props;
+  const { maxNumberOfItems } = props;
 
-	const initialState = (): IState => ({
-		randomNumber: 0,
-		isCorrect: true,
-	});
-	const [state, dispatch] = React.useReducer(reducer, initialState);
+  const initialState = (): IState => ({
+    randomNumber: 0,
+    isCorrect: true,
+    isHuman: false
+  });
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-	React.useEffect(() => {
-		dispatch({
-			randomNumber: Math.floor(Math.random() * (maxNumberOfItems - 1)) + 1,
-			isCorrect: !!Math.floor(Math.random() * 2),
-		});
-	}, [maxNumberOfItems]);
+  React.useEffect(() => {
+    dispatch({
+      randomNumber: Math.floor(Math.random() * (maxNumberOfItems - 1)) + 1,
+      isCorrect: !!Math.floor(Math.random() * 2)
+    });
+  }, [maxNumberOfItems]);
 
-	const { randomNumber, isCorrect } = state;
+  React.useEffect(() => {
+    console.log("IS HUMAN", state.isHuman);
+  });
 
-	return (
-		<Wrapper>
-			<CaptchaTitle randomNumber={randomNumber} isCorrect={isCorrect} />
-			<CaptchaContent
-				randomNumber={randomNumber}
-				isCorrect={isCorrect}
-				correctData={correctData["en"]}
-				uncorrectData={uncorrectData["en"]}
-				maxNumberOfItems={maxNumberOfItems}
-			/>
-		</Wrapper>
-	);
+  function changeIsHuman(isHuman: boolean) {
+    dispatch({
+      isHuman
+    });
+  }
+
+  const { randomNumber, isCorrect, isHuman } = state;
+
+  return (
+    <Wrapper>
+      <CaptchaTitle randomNumber={randomNumber} isCorrect={isCorrect} />
+      <CaptchaContent
+        randomNumber={randomNumber}
+        isCorrect={isCorrect}
+        correctData={correctData["en"]}
+        uncorrectData={uncorrectData["en"]}
+        maxNumberOfItems={maxNumberOfItems}
+        isHuman={isHuman}
+        setIsHuman={changeIsHuman}
+      />
+    </Wrapper>
+  );
 };
 
-export default Captcha;
+export default React.memo(Captcha);
